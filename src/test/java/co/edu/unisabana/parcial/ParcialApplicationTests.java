2997a1641644ffecb356a056f2c31c45108ec06f
juancego <140413383+juancego@users.noreply.github.com>
2024-09-30 07:56:37 -0500

    Prueba unitaria ParcialApplication

--- src/test/java/co/edu/unisabana/parcial/ParcialApplicationTests.java
+++ src/test/java/co/edu/unisabana/parcial/ParcialApplicationTests.java
@@ -4,12 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 @ActiveProfiles("test")
 @SpringBootTest
 class ParcialApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+        assertDoesNotThrow(() -> ParcialApplication.main(new String[] {}));
+    }
 }
