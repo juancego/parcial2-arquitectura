5d10a0d8cd49b39416d7b1e37b1af0c2295aabe5
juancego <140413383+juancego@users.noreply.github.com>
2024-09-30 07:34:24 -0500

    Activar el perfil test para las Unit Test

--- src/test/java/co/edu/unisabana/parcial/ParcialApplicationTests.java
+++ src/test/java/co/edu/unisabana/parcial/ParcialApplicationTests.java
@@ -2,7 +2,9 @@ package co.edu.unisabana.parcial;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class ParcialApplicationTests {
 
